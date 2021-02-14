b3c6e79fdfdc5a75b93a6dfef6f580705f1e08e6
Juan Ricardo <79026814+JRicar2@users.noreply.github.com>
2021-02-13 20:22:03 -0400

    Tarea 1

---
+++ Tarea 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangulo1 = new Rectangle();
+            rectangulo1.basis = 3;
+            rectangulo1.height = 7;
+
+            //rectangulo1.Grow(10);
+
+            Console.WriteLine("Area y Perimetro del Rectangulo 1: ");
+            Console.WriteLine("Area: "+rectangulo1.Area()+"m²");
+            Console.WriteLine("Perimetro: "+rectangulo1.Perimeter()+"m");
+            Console.WriteLine(" ");
+
+            Rectangle rectangulo2 = new Rectangle();
+            rectangulo2.basis = 1.5;
+            rectangulo2.height = 4;
+
+            Console.WriteLine("Area y Perimetro del Rectangulo 2: ");
+            Console.WriteLine("Area: " + rectangulo2.Area() + "m²");
+            Console.WriteLine("Perimetro: " + rectangulo2.Perimeter() + "m");
+            Console.WriteLine(" ");
+
+            Console.Write("Parametro Comparar: ");
+            Console.WriteLine(rectangulo1.Compare(rectangulo2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tarea 1/README.md
@@ -0,0 +1,10 @@
+# Tarea-1
+Replicar el ejercicio del video
+
+<a href="https://www.youtube.com/watch?v=3Lm9AXbCtOQ" rel="nofollow">Métodos de clase (no estáticos). La palabra clave this</a>
+
+Usando GitHub compartir el enlace
+
+Pueden cambiar el ejercicio a (Triángulos equilátero, escaleno, isósceles).
+
+El codigo principal del programa es el "Program.cs" y la clase es "Rectangle.cs", en "Bin" estara el ejecutable.
